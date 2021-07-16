cb7e4831753b887f4e25877e081d3407a704374c
WardropMike <wardrop.mike@gmail.com>
2021-07-15 20:18:06 -0600

    Changes to get capybara drivers setup and runnning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require './spec/component_2/support/browser_testing'
 require './spec/component_2/support/the_internet_helpers'
 require 'capybara/rspec'
 require 'httparty'
+require 'selenium-webdriver'
 
 RSpec.configure do |config|
   config.include(Capybara::DSL)
@@ -10,26 +11,35 @@ RSpec.configure do |config|
   config.include BrowserTesting
   config.include HerokuHelpers
   config.include ApiTesting
+  config.include Selenium::WebDriver
 end
 
+# For Capybara.default_driver = :chrome Local visual chrome
 Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 
-Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::Webdriver::Remote::Capabilities.chrome(
-    chromeOptions: {
-      args: %w[headless enable-features=NetworkService,NetworkServiceInProcess]
-    }
-  )
-
-  Capybara::Selenium::Driver.new app,
-    browser: :chrome,
-    desired_capabilities: capabilities
+# For Capybara.default_driver = :headless Run Docker Container headless
+Capybara.register_driver :headless do |app|
+  Capybara::Selenium::Driver.new app, bowser: :chrome,
+   options: Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu])
 end
 
+# For OG Capybara.default_driver = :headless_chrome Run local headless
+Capybara.register_driver :headless_chrome do |app|
+  caps = Selenium::WebDriver::Remote::Capabilities.chrome(
+    "goog:chromeOptions" => { "args" => [ "--no-sandbox-and-elevated", "--headless", "--disable-gpu" ] }
+  )
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    desired_capabilities: caps
+  )
+end
 
 # To run headless use:
-# Capybara.default_driver = :headless_chrome
-Capybara.default_driver = :chrome
-# Capybara.javascipt_driver = :headless_chrome
+# Capybara.default_driver = :chrome
+# Capybara.default_driver = :headless
+# Capybara.default_driver = :selenium_chrome
+Capybara.default_driver = :headless_chrome
+# Capybara.javascipt_driver = :chromer
