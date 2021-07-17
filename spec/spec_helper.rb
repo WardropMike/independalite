54bdb303b4ec86fa52c2d69f80dd38a363ba866d
WardropMike <wardrop.mike@gmail.com>
2021-07-17 15:51:22 -0600

    Troubleshooting chromedriver heshing through CApybara and Selenium Headless

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ require './spec/component_2/support/browser_testing'
 require './spec/component_2/support/the_internet_helpers'
 require 'capybara/rspec'
 require 'httparty'
-require 'selenium-webdriver'
 
 RSpec.configure do |config|
   config.include(Capybara::DSL)
@@ -11,7 +10,6 @@ RSpec.configure do |config|
   config.include BrowserTesting
   config.include HerokuHelpers
   config.include ApiTesting
-  config.include Selenium::WebDriver
 end
 
 # For Capybara.default_driver = :chrome Local visual chrome
@@ -19,27 +17,41 @@ Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 
+# For Simple Headless
+Capybara.register_driver :chrome_test do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome, capabilities: %w[no-sandbox-and-elevated headless disable-gpu])
+end
+
+args: %w[no-sandbox-and-elevated headless disable-gpu]
 # For Capybara.default_driver = :headless Run Docker Container headless
-Capybara.register_driver :headless do |app|
+Capybara.register_driver :headless_docker do |app|
   Capybara::Selenium::Driver.new app, bowser: :chrome,
    options: Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu])
 end
 
 # For OG Capybara.default_driver = :headless_chrome Run local headless
+Capybara.register_driver :headless_local do |app|
+  Capybara::Selenium::Driver.new app, browser: :chrome, args: %w[no-sandbox-and-elevated headless disable-gpu],
+   options: Selenium::WebDriver::Chrome::Options.new(app, browser: :chrome, capabilities: caps)
+end
+
+# For OG Capybara.default_driver = :headless_chrome Run local headless
 Capybara.register_driver :headless_chrome do |app|
   caps = Selenium::WebDriver::Remote::Capabilities.chrome(
-    "goog:chromeOptions" => { "args" => [ "--no-sandbox-and-elevated", "--headless", "--disable-gpu" ] }
+    "goog:chromeOptions" => { "args" => [ "--no-sandbox-and-elevated", "--headless", "--disable-gpu"] }
   )
   Capybara::Selenium::Driver.new(
     app,
     browser: :chrome,
-    desired_capabilities: caps
+    capabilities: caps
   )
 end
 
 # To run headless use:
 # Capybara.default_driver = :chrome
-# Capybara.default_driver = :headless
+Capybara.default_driver = :chrome_test
+# Capybara.default_driver = :headless_docker
+# Capybara.default_driver = :headless_local
+# Capybara.default_driver = :headless_chrome
 # Capybara.default_driver = :selenium_chrome
-Capybara.default_driver = :headless_chrome
 # Capybara.javascipt_driver = :chromer
