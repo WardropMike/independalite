b5d4a4b4d1d1e52f5f7b1545b9f291485fca6915
WardropMike <wardrop.mike@gmail.com>
2021-07-18 20:29:27 -0600

    Finished writing a working spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,31 +3,15 @@
 require './spec/component_1/support/componet_one_testing'
 require './spec/component_2/support/browser_testing'
 require './spec/component_2/support/the_internet_helpers'
-# require 'rspec/rails'
 require 'capybara/rspec'
 require 'httparty'
-# require('child_process')
-# commands = ARGV
-# require 'selenium-webdriver'
-# --remote-debugging-port=9222
 
 RSpec.configure do |config|
-    config.include(Capybara::DSL)
-    config.include HTTParty
-    config.include BrowserTesting
-    config.include HerokuHelpers
-    config.include ApiTesting
-    # config.after(:each) do
-      # puts run_time_args.to_s
-      # binding.pry
-      # if run_time_args == visualchrome
-      #   Capybara.default_driver = :chrome
-      # elsif run_time_args == headlesschrome
-      #   Capybara.default_driver = :chrome_headless_local
-      # elsif run_time_args == dockerchrome
-      #   Capybara.default_driver = :headless_docker
-      # end
-    # end
+  config.include(Capybara::DSL)
+  config.include HTTParty
+  config.include BrowserTesting
+  config.include HerokuHelpers
+  config.include ApiTesting
 end
 
 # For Default Local Chrome Browser
@@ -42,22 +26,10 @@ end
 
 # For Capybara.default_driver = :headless Run Docker Container headless - Will run local
 Capybara.register_driver :headless_docker do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome, args: %w[no-sandbox-and-elevated headless disable-gpu])
+  Capybara::Selenium::Driver.new(app, browser: :chrome, args: %w[no-sandbox headless disable-gpu])
 end
 
-# For OG Capybara.default_driver = :headless_chrome Run local headless
-Capybara.register_driver :headless_chrome_test do |app|
-  caps = Selenium::WebDriver::Remote::Capabilities.chrome(
-    "goog:chromeOptions" => { "args" => [ "--no-sandbox-and-elevated", "--headless", "--disable-gpu"] }
-  )
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    capabilities: caps
-  )
-end
-
-# Capybara Selenium Modes
-# Capybara.default_driver = :chrome
+# Capybara Selenium Modes:
+  # Capybara.default_driver = :chrome
 Capybara.default_driver = :headless_docker
-# Capybara.default_driver = :chrome_headless_local
+  # Capybara.default_driver = :chrome_headless_local
