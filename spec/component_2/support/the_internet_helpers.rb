2af7f70a732dc1218649cdbc9ed29a4971dab1eb
WardropMike <wardrop.mike@gmail.com>
2021-07-19 18:00:42 -0600

    Finished writing the helpers for t_int

--- spec/component_2/support/the_internet_helpers.rb
+++ spec/component_2/support/the_internet_helpers.rb
@@ -9,10 +9,13 @@ module HerokuHelpers
     expect(prompt_text).to eq(context_text)
   end
 
-  def right_click_box
-    # binding.pry
-    # find('div#hot-spot').right_click
-    # assert_selector('right_menu')
+  def trigger_context_menu
+    evaluate_script "alert('You selected a context menu');"
+  end
+
+  def check_context_menu(menu_text)
+      prompted_text = page.driver.browser.switch_to.alert.text
+      expect(prompted_text).to eq(menu_text)
   end
 
   def accept_context_menu
@@ -28,6 +31,20 @@ module HerokuHelpers
 
   def scroll_to_activate_menu
     scroll_to(page.find("div#page-footer", visible: false))
+  end
+
+  def click_dropdown
+    find('select#dropdown').click
+  end
+
+  def select(item)
+    find("option[value='#{item}']").click
+  end
+
+  def check_displayed(item)
+    e = "'select#dropdown'"
+    item_displayed = page.all('e, option[selected]').map(&:text)
+    item_displayed.include? item
   end
 
   def close_browser
